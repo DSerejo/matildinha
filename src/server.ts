cce235dc9f44eefabc42a68586176face5e8177e
Denny Serejo <denny_serejo@hotmail.com>
2021-05-28 04:45:24 +0000

    adding angular

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import path from "path";
 // rest of the code remains same
+const publicPath = process.env.NODE_ENV == 'production'? path.join(__dirname, 'ng'): path.join(__dirname, '../ng/dist/ng');
 const app = express();
 const PORT =  process.env.PORT || 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.use(express.static(publicPath))
+  .set('views', publicPath)
+app.get('/', (req, res) => res.render('index'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
